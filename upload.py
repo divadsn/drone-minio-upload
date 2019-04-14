f33d5b48c9e8e3a136bc0b6b65be55897cc35de6
David Sn <divad.nnamtdeis@gmail.com>
2019-04-14 05:24:44 +0200

    Start rewrite of the plugin to Python

---
+++ upload.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Minio client dependencies
+from minio import Minio
+from minio.policy import Policy
+
+# TODO: Rewrite upload script with Minio
+# TODO: Find a way to read Drone env
