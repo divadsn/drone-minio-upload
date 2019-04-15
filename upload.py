4a3241fb0bddb164d7e94d3c6d306b0a7dace353
David Sn <divad.nnamtdeis@gmail.com>
2019-04-15 22:23:06 +0200

    Save my workstate from Linux

--- upload.py
+++ upload.py
@@ -1,10 +1,15 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 
 # Minio client dependencies
 from minio import Minio
 from minio.policy import Policy
+
+S3_BUCKET = os.environ.get("S3_BUCKET", "bucket")
+S3_HOST = os.environ.get("S3_HOST", "play.minio.io")
+S3_KEY = os.environ.get("S3_KEY", "Q3AM3UQ867SPQQA43P2F")
+S3_SECRET = os.environ.get("S3_SECRET", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
 
 # TODO: Rewrite upload script with Minio
 # TODO: Find a way to read Drone env
